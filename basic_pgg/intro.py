c68d127a39c91e76012983a7491246614b4407c3
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2019-10-10 20:30:29 +0200

    intro, print i typy

---
+++ basic_pgg/intro.py
@@ -0,0 +1,22 @@
+# To jest komentarz
+# kolejna linijka
+
+# TYPY DANYCH
+# str - string
+print("Hello world!")
+print('Hello world!')
+
+# int
+print(10)
+print(-8)
+print(123)
+
+# float
+print(3.5)
+print(-2.8)
+print(5.0)
+print(10.0)
+print(10.) # 10.0
+print(.5)
+
+
