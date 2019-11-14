dd0607d47daa788c76fe08bcb8dab83f47680d40
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2019-11-14 18:58:11 +0100

    Spotkanie #9 - zad4

--- functions_pgg/zad_4.py
+++ functions_pgg/zad_4.py
@@ -22,11 +22,11 @@ def formatuj(*args, **kwargs):
 
     return '\n'.join(wynik)
 
-# formatuj('Hello world!') == 'Hello world!'
-# formatuj('Hello world!', 'Ala ma kota') == 'Hello world!\nAla ma kota'
-# formatuj('Hello world!', imie='Ala') == 'Hello world!'
-# formatuj('Hello $imie!', imie='Ala') == 'Hello Ala!'
-# formatuj('Hello $imie, $nazwisko!', imie='Ala') == 'Hello Ala, $nazwisko!'
-# formatuj('Hello $imie, $nazwisko!', imie='Ala', nazwisko='Kowalski')
-# == 'Hello Ala, Kowalski!'
+def testy_zbiorcze():
+    assert formatuj('Hello world!') == 'Hello world!'
+    assert formatuj('Hello world!', 'Ala ma kota') == 'Hello world!\nAla ma kota'
+    assert formatuj('Hello world!', imie='Ala') == 'Hello world!'
+    assert formatuj('Hello $imie!', imie='Ala') == 'Hello Ala!'
+    assert formatuj('Hello $imie, $nazwisko!', imie='Ala') == 'Hello Ala, $nazwisko!'
+    assert formatuj('Hello $imie, $nazwisko!', imie='Ala', nazwisko='Kowalski') == 'Hello Ala, Kowalski!'
 
