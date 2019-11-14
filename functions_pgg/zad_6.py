21e4faa1b38e5a9db49443bdf78b60990948ea63
Piotr Grabski-Gradziński <piotr.gradzinski@gmail.com>
2019-11-14 20:05:12 +0100

    Spotkanie #9 - zad6 - splaszcz

--- functions_pgg/zad_6.py
+++ functions_pgg/zad_6.py
@@ -21,4 +21,22 @@ def splaszcz(lista):
 
 def test_splaszcz():
     assert splaszcz( [ 1, 2, 3, [4,5] ] ) == [1,2,3,4,5]
+    assert splaszcz([]) == []
+    assert splaszcz([1,2,3,4,5]) == [1,2,3,4,5]
+    assert splaszcz([1, 2, 3, [4, 5, [6]], 7]) == [1, 2, 3, 4, 5, 6, 7]
+    assert splaszcz([1, 2, 'a', 3.5]) == [1, 2, 'a', 3.5]
+    assert splaszcz([1, 2, 3, [], 4, 5, 6]) == [1, 2, 3, 4, 5, 6]
+    assert splaszcz([[[1,[2,3,[4,[5,6]]]],[7]],[8,9]]) == [1,2,3,4,5,6,7,8,9]
+
+import pytest
+
+def test_splaszcz_nie_z_lista():
+    with pytest.raises(TypeError):
+        splaszcz(1)
+
+    with pytest.raises(TypeError):
+        splaszcz('ala ma kota')
+
+    with pytest.raises(TypeError):
+        splaszcz({1,2,3,4,5})
 
